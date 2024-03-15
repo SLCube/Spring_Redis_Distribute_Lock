ddc0de18036947e1acb6153282fb7e7054dde5b6
SLCube <kgm0096@naver.com>
2024-03-15 14:00:51 +0900

    Initial commit

---
+++ src/main/java/com/example/distribute_lock/SpringRedisDistributeLockApplication.java
@@ -0,0 +1,13 @@
+package com.example.distribute_lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisDistributeLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisDistributeLockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/distribute_lock/SpringRedisDistributeLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distribute_lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisDistributeLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
