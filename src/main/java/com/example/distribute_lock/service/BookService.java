0bdbbd1fafefc07ab3fafcd6f1c52d6e45ed599b
SLCube <kgm0096@naver.com>
2024-03-15 16:21:28 +0900

    facade를 이용해 분산락 로직을 분리 테스트코드 통과

--- src/main/java/com/example/distribute_lock/service/BookService.java
+++ src/main/java/com/example/distribute_lock/service/BookService.java
@@ -4,12 +4,9 @@ import com.example.distribute_lock.entity.Book;
 import com.example.distribute_lock.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
@@ -21,27 +18,8 @@ public class BookService {
     private final RedissonClient redissonClient;
 
     public void purchase(final Long bookId, final long quantity) {
-        RLock lock = redissonClient.getLock(String.format("purchase:book:%d", bookId));
-
-        try {
-            boolean available = lock.tryLock(10, 1, TimeUnit.SECONDS);
-
-            if (!available) {
-                log.info("redisson getLock timeout");
-                return;
-            }
-
-            Book book = bookRepository.findById(bookId)
-                    .orElseThrow(IllegalArgumentException::new);
-
-            book.purchase(quantity);
-
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            lock.unlock();
-        }
-
-
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(IllegalArgumentException::new);
+        book.purchase(quantity);
     }
 }
